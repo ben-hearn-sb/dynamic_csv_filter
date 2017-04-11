a1be1400e25678d94cd94807c1a81eb4d9007801
Ben <ben.hearn@starbreeze.com>
2017-04-11 21:28:53 +0200

    better logic implementation and removed un-used functions

--- general_utils.cpp
+++ general_utils.cpp
@@ -1,23 +1,8 @@
 #include "general_utils.h"
 
-std::vector<std::string> general_utils::extractHeaders(std::vector<std::vector<std::string>>& inputVec)
-{
-    // Not used but useful to keep around for reference
-    std::vector<std::vector<std::string>>::iterator row;
-    std::vector<std::string>::iterator col;
-    std::vector<std::string> headers;
-    int firstElement = 0;
-
-    for (row = inputVec.begin(); row != inputVec.end(); ++row)
-    {
-        col = row->begin(); // Get the first element of the vector
-        headers.push_back(*col);
-    }
-    return headers;
-}
-
 std::vector<const char*> general_utils::convertToConstCharVec(std::vector<std::string>& vs)
 {
+    // Converts a std::string vector to a char* vector
     std::vector<const char*> vc;
     std::transform(vs.begin(), vs.end(), std::back_inserter(vc), convertToCharStar);
     return vc;
@@ -25,5 +10,6 @@ std::vector<const char*> general_utils::convertToConstCharVec(std::vector<std::s
 
 const char* general_utils::convertToCharStar(const std::string & s)
 {
+    // Necessary function for the above convertToConstCharVec to work
     return s.c_str();
 }
